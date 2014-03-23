97e1567fd6bf544c5acef2cb732ca6dbdf575c94
Mohammad El-Abid <mohammad@el-abid.com>
2014-03-23 16:23:27 -0400

    Change Java logic to not copy board, but mark all live cells.

--- java-src/edu/bridgeport/melabid/cs301/life/Board.java
+++ java-src/edu/bridgeport/melabid/cs301/life/Board.java
@@ -1,23 +1,23 @@
 /*
-Copyright (c) 2014 Mohammad El-Abid
+ Copyright (c) 2014 Mohammad El-Abid
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
 */
 
 package edu.bridgeport.melabid.cs301.life;
@@ -36,11 +36,7 @@ public class Board {
 	}
 
 	public void nextGeneration() {
-		// boolean[][] nextBoard = board;
 		boolean[][] nextBoard = new boolean[board.length][board[0].length];
-		for(int i = 0; i < board.length; i++) {
-			System.arraycopy(board[i], 0, nextBoard[i], 0, board[0].length);
-		}
 
 		for(int row = 0; row < board.length; row++) {
 			for(int column = 0; column < board[row].length; column++) {
@@ -67,8 +63,8 @@ public class Board {
 
 				// life and death logic
 				if(board[row][column]) { // if alive
-					if(neighbors != 2 && neighbors != 3) {
-						nextBoard[row][column] = false; // death
+					if(neighbors == 2 || neighbors == 3) { // if still alive
+						nextBoard[row][column] = true;
 					}
 				} else {
 					if(neighbors == 3) nextBoard[row][column] = true;
